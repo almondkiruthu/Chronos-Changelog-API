7c7eb4bc4c2f22eed8df1668e4cd87ba6bbee094
almondkiruthu <113090717+almondkiruthu@users.noreply.github.com>
2023-10-04 14:00:06 +0300

    Created routes and imported them in the app server'

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-
+import router from './router';
 const app = express();
 
 app.get('/', (req, res) => {
   console.log('Hello from epress');
   res.json({ message: 'Hello World' });
 });
+
+app.use('/api', router);
 
 export default app;
